f6162245a55e812485a140ebe3200c30bd08afdb
Daniel Gallegos <dgalort@stanford.edu>
2017-05-10 17:34:17 -0700

    blah

--- blobDetection.py
+++ blobDetection.py
@@ -3,32 +3,47 @@ import cv2
 import numpy as np;
 
 # Read image
-im = cv2.imread("data/milestone.jpg", cv2.IMREAD_GRAYSCALE)
+def findBlob(im):
+    # Set up the detector with default parameters.
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByColor = True
+    params.blobColor = 255
+    params.filterByArea = True
+    params.minArea = 0
+    params.maxArea = im.shape[0] * im.shape[1] * 10
 
-# Set up the detector with default parameters.
-params = cv2.SimpleBlobDetector_Params()
-params.filterByColor = True
-params.blobColor = 255
-params.filterByArea = True
-params.minArea = 0
-params.maxArea = im.shape[0] * im.shape[1] * 10
+    params.filterByCircularity = False
 
-params.filterByCircularity = False
+    params.filterByInertia = False
+    params.filterByConvexity = False
 
-params.filterByInertia = False
-params.filterByConvexity = False
+    params.minDistBetweenBlobs = 0
 
-params.minDistBetweenBlobs = 5
+    detector = cv2.SimpleBlobDetector(params)
 
-detector = cv2.SimpleBlobDetector(params)
+    # Detect blobs.
+    keypoints = detector.detect(im)
+    
+    return keypoints
 
-# Detect blobs.
-keypoints = detector.detect(im)
+def getKeypointIm(im, keypoints):
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return im_with_keypoints
 
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+def showKeypoints(im_with_keypoints):
+    # Show keypoints
+    cv2.imshow("Keypoints", im_with_keypoints)
+    cv2.waitKey(0)
 
-# Show keypoints
-cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
+
+def main():
+    im = cv2.imread("data/milestone.jpg", cv2.IMREAD_GRAYSCALE)
+
+    keypoints = findBlob(im)
+    keypointIm = getKeypointIm(im, keypoints)
+    showKeypoints(keypointIm)
+
+if __name__ == "__main__":
+    main()
--- driver.py
+++ driver.py
@@ -1,109 +1,26 @@
 import numpy as np
 import cv2
-# import cvxpy as cvx
+import blobDetection
 
-orig_img = cv2.imread('data/milestone.jpg')
+cap = cv2.VideoCapture('data/flashlight.mp4')
 
-# Blur image to remove noise
-frame=cv2.GaussianBlur(orig_img, (3, 3), 0)
-frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-# Switch image from BGR colorspace to HSV
-# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    if not ret: break
 
-# cv2.imshow("Keypoints", hsv)
-# cv2.waitKey(0)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# define range of purple color in HSV
-# purpleMin = (115,50,10)
-# purpleMax = (160, 255, 255)
+    keypoints = findBlob(gray)
+    gray = getKeypointIm(gray, keypoints)
 
-# print hsv.shape
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# Sets pixels to white if in purple range, else will be set to black
-frame = cv2.inRange(frame, 200, 255)
-
-minSize = 10
-
-for size in range(minSize, min(frame.shape) / 2):
-    for 
-
-
-cv2.imshow("Keypoints", frame)
-cv2.waitKey(0)
-
-
-'''
-x1 = cvx.Variable(2)
-x2 = cvx.Variable(2)
-
-A = frame / float(255)
-A = A.astype(int)
-
-constraints = [x1 >= 0,
-               x2 >= 0,
-               x1[0] <= frame.shape[0],
-               x1[1] <= frame.shape[1],
-               x2[0] <= frame.shape[0],
-               x2[1] <= frame.shape[1]]
-# lam = cvx.Parameter(sign = 'positive', 1)
-'''
-
-# cv2.imshow("Keypoints", mask)
-# cv2.waitKey(0)
-
-# Bitwise-AND of mask and purple only image - only used for display
-# res = cv2.bitwise_and(frame, frame, mask= mask)
-
-#    mask = cv2.erode(mask, None, iterations=1)
-# commented out erode call, detection more accurate without it
-
-# dilate makes the in range areas larger
-# frame = cv2.dilate(frame, None, iterations=1)
-#
-# cv2.imshow("Keypoints", frame)
-# cv2.waitKey(0)
-'''
-    # Set up the SimpleBlobdetector with default parameters.
-params = cv2.SimpleBlobDetector_Params()
-
-# Change thresholds
-params.minThreshold = 50;
-params.maxThreshold = 256;
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 30
-params.maxArea = 100000
-
-# Filter by Circularity
-# params.filterByCircularity = True
-# params.minCircularity = 0.1
-
-# Filter by Convexity
-# params.filterByConvexity = True
-# params.minConvexity = 0.5
-
-# Filter by Inertia
-# params.filterByInertia =True
-# params.minInertiaRatio = 0.5
-
-params.filterByColor = True
-params.blobColor = 255
-
-detector = cv2.SimpleBlobDetector(params)
-
-# Detect blobs.
-# frame=255-frame
-#
-cv2.imshow("Keypoints", frame)
-cv2.waitKey(0)
-keypoints = detector.detect(frame)
-
-print keypoints
-im_with_keypoints = cv2.drawKeypoints(orig_img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-# Show keypoints
-cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
-'''
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
