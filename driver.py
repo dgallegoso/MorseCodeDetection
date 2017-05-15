ad39d452b1185303f57b20dba916d10a8fc1d32c
Daniel Gallegos <dgalort@stanford.edu>
2017-05-15 16:42:42 -0700

    driver update

--- driver.py
+++ driver.py
@@ -6,18 +6,15 @@ cap = cv2.VideoCapture('data/flashlight.mp4')
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, im = cap.read()
 
     if not ret: break
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    keypoints = blobDetection.findBlob(gray)
-    gray = blobDetection.getKeypointIm(gray, keypoints)
+    keypoints = blobDetection.findBlob(im)
+    im = blobDetection.getKeypointIm(im, keypoints)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',im)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
