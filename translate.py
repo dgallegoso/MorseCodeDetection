9934874d4a2cad8ddc17c7c2f40bd0afcdbc9466
jobalia <sarahnjobalia@gmail.com>
2017-05-15 18:18:46 -0700

    parses on/off array by averages and returns a dots/dash/spaces array

---
+++ translate.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Standard imports
+import cv2
+import numpy as np;
+import matplotlib.pyplot as plt
+
+# Read image
+def parse(nums):
+ 
+    loc = np.hstack((0,np.where(nums[:-1] != nums[1:])[0])) + 1
+    intervals = np.add.reduceat(nums, loc)
+    
+    on = intervals[intervals > 0]
+    mean_on = np.mean(on)
+    
+    off = intervals[intervals < 0]
+    mean_off = np.mean(off)
+
+    np.place(intervals, intervals<mean_off, 0)
+    intervals = intervals[intervals >= 0]
+    np.place(intervals, intervals<mean_off, 0)
+    np.place(intervals, np.logical_and(intervals>0, intervals<=mean_on), 1)
+    np.place(intervals, intervals>1, 2)
+
+
+    return intervals
+
+
+
+def main():
+    nums = np.array([-1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
+    print parse(nums)
+
+if __name__ == "__main__":
+    main()
